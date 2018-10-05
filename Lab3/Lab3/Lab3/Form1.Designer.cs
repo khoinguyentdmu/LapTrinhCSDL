cd0bf67cb63e44341dd427f8b81fa397b54f72bb
Khoi <khoi.nguyen.tdmu@gmail.com>
2018-10-05 10:46:37 +0700

    Lab 3

---
+++ Lab3/Lab3/Lab3/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboMaKhoa = new System.Windows.Forms.ComboBox();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnThoat);
+            this.panel1.Controls.Add(this.btnReport);
+            this.panel1.Controls.Add(this.cboMaKhoa);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn Khoa:";
+            // 
+            // cboMaKhoa
+            // 
+            this.cboMaKhoa.FormattingEnabled = true;
+            this.cboMaKhoa.Location = new System.Drawing.Point(81, 17);
+            this.cboMaKhoa.Name = "cboMaKhoa";
+            this.cboMaKhoa.Size = new System.Drawing.Size(160, 21);
+            this.cboMaKhoa.TabIndex = 1;
+            // 
+            // btnReport
+            // 
+            this.btnReport.BackColor = System.Drawing.Color.Lime;
+            this.btnReport.Location = new System.Drawing.Point(247, 15);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(75, 23);
+            this.btnReport.TabIndex = 2;
+            this.btnReport.Text = "Xuất Report";
+            this.btnReport.UseVisualStyleBackColor = false;
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.BackColor = System.Drawing.Color.Red;
+            this.btnThoat.Location = new System.Drawing.Point(328, 15);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 2;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = false;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.crystalReportViewer1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 48);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 402);
+            this.panel2.TabIndex = 1;
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 402);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Thống kê thông tin sinh viên";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.ComboBox cboMaKhoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+    }
+}
+
---
+++ Lab3/Lab3/Lab3/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnn;
+        SqlDataAdapter da;
+        DataTable dt_KHOA;
+        DataTable dt_SINHVIEN;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cnn = new SqlConnection("Data Source=.;Initial Catalog=QLSINHVIEN4;Integrated Security=True");
+
+            da = new SqlDataAdapter("Select * from KHOA", cnn);
+            dt_KHOA = new DataTable();
+            da.Fill(dt_KHOA);
+
+            da.SelectCommand.CommandText = "Select SV.*, K.MaKhoa, K.TenKhoa, L.TenLop"
+                                            + " From (SINHVIEN SV inner join LOP L on SV.MaLop = L.MaLop)"
+                                            + "inner join KHOA K on L.MaKhoa = K.MaKhoa";
+            dt_SINHVIEN = new DataTable();
+            da.Fill(dt_SINHVIEN);
+
+            cboMaKhoa.DataSource = dt_KHOA;
+            cboMaKhoa.DisplayMember = "TenKhoa";
+            cboMaKhoa.ValueMember = "MaKhoa";
+            cboMaKhoa.SelectedIndex = -1;
+            //cboMaKhoa.Items.Add("All");
+
+            CrystalReport1 rpt = new CrystalReport1();
+            rpt.SetDataSource(dt_SINHVIEN.DefaultView);
+            crystalReportViewer1.ReportSource = rpt;
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            CrystalReport1 rpt = new CrystalReport1();
+            if (cboMaKhoa.Text.Length > 0)
+            {
+                dt_SINHVIEN.DefaultView.RowFilter = "MaKhoa = '" + cboMaKhoa.SelectedValue.ToString() + "'";
+                rpt.SetDataSource(dt_SINHVIEN.DefaultView);
+            }
+            else
+            {
+                rpt.SetDataSource(dt_SINHVIEN.DefaultView);
+            }
+
+            crystalReportViewer1.ReportSource = rpt;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
